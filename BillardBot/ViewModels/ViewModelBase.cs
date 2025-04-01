55860712d8d75ddabe7f4f40fd7a904ba06b1ae2
Daniel Mortensen <damor23@student.sdu.dk>
2025-04-01 21:48:22 +0200

    Created Avalonia Project

---
+++ BillardBot/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace BillardBot.ViewModels;
+
+public class ViewModelBase : ObservableObject
+{
+}
---
+++ BillardBot/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace BillardBot.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
